fec9209a3b703d8078c15c5f4572b5e0f722ee5d
Pranaya Bhat <prbhat@qca-sdc.com>
2018-10-15 15:42:08 -0500

    test: PizzaList if empty

--- src/__tests__/component/PizzaList/index.test.js
+++ src/__tests__/component/PizzaList/index.test.js
@@ -11,4 +11,10 @@ describe("Given RepoList", () => {
     const component = renderPizzaList();
     expect(component.is("div.pizzaList")).to.be.true();
   });
+  describe("when list is empty", () => {
+    it("should render no PizzaItems", () => {
+      const component = renderPizzaList();
+      expect(component.find("li").length).to.equal(0);
+    });
+  });
 });
