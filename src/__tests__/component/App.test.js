3f21a3582352965dc92057ec5d2bf2141ff7b81b
Pranaya Bhat <prbhat@qca-sdc.com>
2018-10-15 14:37:26 -0500

    test: add state 'pizzalist'

--- src/__tests__/component/App.test.js
+++ src/__tests__/component/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { expect } from "code";
 import { shallow } from "enzyme";
-import App from "../../src/component/App";
+import App from "../../../src/component/App";
 
 describe("App", () => {
   let component;
@@ -11,5 +11,9 @@ describe("App", () => {
 
   it("should be a <main>", () => {
     expect(component.type()).to.equal("main");
+  });
+
+  it("should have a default PizzaList state of empty string", () => {
+    expect(component.state().PizzaList).to.equal([]);
   });
 });
--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { fetchPizza } from "../pizza";
 
 class App extends React.Component {
+  state = {
+    PizzaList: []
+  };
+
   render() {
     return <main>Hello</main>;
   }
