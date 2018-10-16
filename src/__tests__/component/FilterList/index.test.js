67baeb588c8050288d2166717485881e2bc354ff
Pranaya Bhat <prbhat@qca-sdc.com>
2018-10-16 14:53:28 -0500

    test: searchText should change on input change

--- src/__tests__/component/FilterList/index.test.js
+++ src/__tests__/component/FilterList/index.test.js
@@ -19,4 +19,20 @@ describe("Given FilterList", () => {
     expect(component.find("button")).to.have.length(1);
     expect(component.find("button").text()).to.equal("Search");
   });
+  it("should have a default state of searchText as empty string", () => {
+    expect(component.state().searchText).to.equal("");
+  });
+  describe("when the input text changes", () => {
+    let mockText;
+    let input;
+    beforeEach(() => {
+      mockText = "hello";
+      input = component.find("input");
+      input.simulate("change", { target: { value: mockText } });
+    });
+
+    it("should update the component state", () => {
+      expect(component.state().searchText).to.equal(mockText);
+    });
+  });
 });
--- src/component/FilterList/index.js
+++ src/component/FilterList/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 class FilterList extends React.Component {
+  state = {
+    searchText: ""
+  };
+
+  handleFilter = e => {
+    const searchText = e.target.value;
+    this.setState({ searchText });
+  };
   render() {
     return (
       <form>
-        <input />
+        <input value={this.state.searchText} onChange={this.handleFilter} />
         <button>Search</button>
       </form>
     );
