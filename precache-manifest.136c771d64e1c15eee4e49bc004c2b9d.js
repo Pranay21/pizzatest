e1b63ac68a1a14637bb6366599c662434a20ba3a
Pranaya Bhat <prbhat@qca-sdc.com>
2018-10-17 09:33:52 -0500

    Updates

---
+++ precache-manifest.136c771d64e1c15eee4e49bc004c2b9d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8d2168f64fbbf6697cc",
+    "url": "/pizzatest3/static/js/runtime~main.f8d2168f.js"
+  },
+  {
+    "revision": "2cd0329e65eeace1b744",
+    "url": "/pizzatest3/static/js/main.2cd0329e.chunk.js"
+  },
+  {
+    "revision": "97c5b1e20044a98b4d00",
+    "url": "/pizzatest3/static/js/1.97c5b1e2.chunk.js"
+  },
+  {
+    "revision": "28a9331e307ea4f8dfff1d18b77c1513",
+    "url": "/pizzatest3/index.html"
+  }
+];
